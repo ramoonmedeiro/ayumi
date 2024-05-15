1ca41c6a4489b561d682697fb4622e5833343375
Ramon Medeiro <r.medeiro10@gmail.com>
2024-05-15 11:25:08 -0300

    feat: inserindo módulo de crawlers

--- ayumi.py
+++ ayumi.py
@@ -9,6 +9,7 @@ from src.settings import Settings
 
 # src libs
 from src.recon.subdomain_discovery import SubdomainDiscovery
+from src.recon.crawlers import Crawlers
 
 
 print(Fore.RED + Settings.BANNER.value + Style.RESET_ALL)
@@ -20,8 +21,11 @@ subparsers = parser.add_subparsers(dest='command')
 
 # Cria o subparser para o comando 'recon'
 subs_parser = subparsers.add_parser('recon')
-subs_parser.add_argument('-ss', required=True, help='scan subdomains')
-subs_parser.add_argument('-o', required=False, help='output file.')  # Adicionado aqui
+subs_parser.add_argument('-ds', required=False, help='Discovery subdomains.')
+subs_parser.add_argument('-rc', required=False, help='Run crawler (katana).')
+subs_parser.add_argument('-rh', required=False, help='Run history crawler (gau and waybackurls).')
+subs_parser.add_argument('-rp', required=False, help='Run Paramspider.')
+subs_parser.add_argument('-o', required=False, help='output file.')
 
 # Cria o subparser para o comando 'atk'
 atk_parser = subparsers.add_parser('atk')
@@ -30,17 +34,35 @@ atk_parser = subparsers.add_parser('atk')
 # Analisa os argumentos da linha de comando
 args = parser.parse_args()
 
-# if not args.no_banner:
-#    print(Fore.RED + Settings.BANNER.value + Style.RESET_ALL)
-
 if args.command not in ['recon', 'atk']:
     parser.error("Comando inválido. Os comandos válidos são 'recon' e 'atk'.")
 
 if args.command == 'recon':
     print(Fore.LIGHTMAGENTA_EX + "🌸 RECON MODE" + Style.RESET_ALL)
-    if args.ss:
+    if args.ds:
         print(Fore.MAGENTA + "🌿 Running subdomain discovery\n" + Style.RESET_ALL)
-        subdomain_discovery = SubdomainDiscovery(domain_file=args.ss)
+        subdomain_discovery = SubdomainDiscovery(domain_file=args.ds)
         subdomain_discovery.run_all(output_file=args.o)
         print(Fore.MAGENTA + "🌿 Subdomain discovery finished" + Style.RESET_ALL)
+        exit(0)
+
+    if args.rc:
+        print(Fore.MAGENTA + "🌿 Running crawler\n" + Style.RESET_ALL)
+        crawler = Crawlers(domains_file=args.rc)
+        crawler.run_katana(output_file=args.o)
+        print(Fore.MAGENTA + "🌿 Crawler finished" + Style.RESET_ALL)
+        exit(0)
+
+    if args.rh:
+        print(Fore.MAGENTA + "🌿 Running history crawler\n" + Style.RESET_ALL)
+        crawler = Crawlers(domains_file=args.rh)
+        crawler.run_history(output_file=args.o)
+        print(Fore.MAGENTA + "🌿 History crawler finished" + Style.RESET_ALL)
+        exit(0)
+
+    if args.rp:
+        print(Fore.MAGENTA + "🌿 Running Paramspider\n" + Style.RESET_ALL)
+        crawler = Crawlers(domains_file=args.rp)
+        crawler.run_paramspider(output_file=args.o)
+        print(Fore.MAGENTA + "🌿 Paramspider finished" + Style.RESET_ALL)
         exit(0)
---
+++ src/recon/crawlers.py
@@ -0,0 +1,116 @@
+import subprocess
+from colorama import Fore, Style
+from src.settings import Settings
+
+
+class Crawlers:
+    def __init__(self, domains_file: str) -> None:
+        self.domains_file = domains_file
+        self.PATH_GO = Settings.PATH_GO.value
+
+    def run_process(self, command) -> None:
+
+        try:
+            result = subprocess.run(
+                command,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                text=True
+            )
+
+            if result.returncode != 0:
+                print(f"Error on exec command: {result.stderr}")
+                return
+
+        except Exception as e:
+            print(f"Error on trying running command: {str(e)}")
+
+    def run_katana(self, output_file=None) -> None:
+
+        print(Fore.GREEN + "Running: " + Fore.CYAN + "katana" + Style.RESET_ALL)
+
+        if not output_file:
+            output_file = "crawler.txt"
+
+        command = [
+            f'{self.PATH_GO}/katana',
+            '-list',
+            self.domains_file,
+            '-o',
+            output_file
+        ]
+
+        self.run_process(command)
+        print(Fore.MAGENTA + f"Results saved in {output_file}" + Style.RESET_ALL)
+
+    def run_gau(self) -> None:
+
+        print(Fore.GREEN + "Running: " + Fore.CYAN + "gau" + Style.RESET_ALL)
+
+        command = f"cat {self.domains_file} | {self.PATH_GO}/gau -b png,jpg,gif -t 5 | tee -a gau.txt"
+
+        subprocess.run(
+            command,
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True
+        )
+
+    def run_waybackurls(self) -> None:
+
+        print(Fore.GREEN + "Running: " + Fore.CYAN + "waybackurls" + Style.RESET_ALL)
+
+        command = f"cat {self.domains_file} | {self.PATH_GO}/waybackurls | tee -a waybackurls.txt"
+
+        subprocess.run(
+            command,
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True
+        )
+
+    def parse_results(self, output_file) -> None:
+
+        command = f"cat gau.txt waybackurls.txt | sort | {self.PATH_GO}/anew {output_file} && rm gau.txt waybackurls.txt"
+        subprocess.run(
+            command,
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True
+        )
+
+    def run_history(self, output_file=None) -> None:
+
+        self.run_gau()
+        self.run_waybackurls()
+        if not output_file:
+            output_file = "urls-history.txt"
+        self.parse_results(output_file)
+        print(Fore.MAGENTA + f"Results saved in {output_file}" + Style.RESET_ALL)
+
+    def run_paramspider(self, output_file=None) -> None:
+        command = [
+            'paramspider',
+            '-l',
+            self.domains_file,
+            '-p',
+            'AYUMI'
+        ]
+
+        self.run_process(command)
+
+        if not output_file:
+            output_file = "paramspider.txt"
+
+        command = f"cat results/* | sort | {self.PATH_GO}/anew {output_file} && rm -rf results/"
+        subprocess.run(
+            command,
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True
+        )
+        print(Fore.MAGENTA + f"Results saved in {output_file}" + Style.RESET_ALL)
